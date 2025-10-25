cfce0fbd3829c1098de4ba7807b103300b19df8c
stella5555 <relaryone@gmail.com>
2025-10-25 13:36:20 -0500

    agregando pruebas la librería

---
+++ ejemplos/demo.py
@@ -0,0 +1,12 @@
+
+from estadisticas import EstadisticaCuantitativa, EstadisticaCualitativa
+
+# Prueba para cuantitativa
+datos_numeros = [10, 20, 30, 40, 50]
+estadistica_num = EstadisticaCuantitativa(datos_numeros)
+print("¡Funciona! Media:", estadistica_num.media())
+
+# Prueba para cualitativa  
+datos_colores = ["rojo", "azul", "rojo", "verde"]
+estadistica_color = EstadisticaCualitativa(datos_colores)
+print("Moda:", estadistica_color.moda())
