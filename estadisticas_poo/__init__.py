afcfe0b7615f6e7b5d03da9c50c68affba749429
stella5555 <relaryone@gmail.com>
2025-10-25 13:34:12 -0500

    agregando el código para que se pueda instalar con pip install -e

---
+++ estadisticas_poo/__init__.py
@@ -0,0 +1,20 @@
+"""
+Librería de Estadísticas - POO
+Autores: Hilary Cruz, Maria Chavez, Jean Malvacedo, Piero Mejia
+"""
+
+# Importar todas las clases para que estén disponibles directamente
+from .EstadisticaBase import EstadisticaBase
+from .estadistica_cuantitativa import EstadisticaCuantitativa
+from .estadistica_cualitativa import EstadisticaCualitativa
+
+# Lista de lo que se exporta al hacer: from estadisticas import *
+__all__ = [
+    'EstadisticaBase',
+    'EstadisticaCuantitativa', 
+    'EstadisticaCualitativa'
+]
+
+# Información del paquete
+__version__ = '1.0.0'
+__author__ = 'Equipo Parcial-LP2'
